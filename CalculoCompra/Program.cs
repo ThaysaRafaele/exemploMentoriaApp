dac13cdda31f5adaad37d2127a763224de929bad
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2026-07-20 23:44:19 -0400

    feat: exemplo 2CalculoCompra

---
+++ CalculoCompra/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static double CalcularTotal(double subtotal, double desconto, double frete)
+    {
+        double totalComDesconto = subtotal - desconto;
+        return totalComDesconto + frete;
+    }
+
+    static void Main()
+    {
+        double subtotal = 200;
+        double desconto = 10; // desconto de 10%
+        double frete = 15;
+
+        double total = CalcularTotal(subtotal, desconto, frete);
+        Console.WriteLine($"Total a pagar: {total}");
+    }
+}
