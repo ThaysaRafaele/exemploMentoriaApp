24cbdda932b7c2027f26947d4fca5f784221ea3b
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2026-07-21 00:05:36 -0400

    feat: exemplo 3 - calculo de média

---
+++ CalculoMediaExemplo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] notas = { 8, 7, 9, 10 }
+        int soma = 0;
+
+
+        for (int i = 0; i <= notas.Length; i++)
+        {
+            soma += notas[i];
+        }
+
+
+        double media = soma / notas.Length;
+        Console.WriteLine("A média é: " + media);
+    }
+}
